632ab94ce507b5271effc02f6ed20a0a4d2d6ffe
Joaquin Villalba <joaquinvillalba8@gmail.com>
2024-03-16 17:15:26 -0300

    first commit

---
+++ backend-desapp-api/src/main/java/ar/edu/unq/desapp/grupoD/backenddesappapi/model/User.java
@@ -0,0 +1,45 @@
+package ar.edu.unq.desapp.grupoD.backenddesappapi.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @NotBlank
+    @Size(min = 3, max = 30)
+    private String name;
+
+    @NotBlank
+    @Size(min = 3, max = 30)
+    private String lastName;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 10, max = 30)
+    private String direction;
+
+    //seguramente no utilizemos el pattern xq se cifraran las contraseñas y ahi se hara la validacion
+    // al menos 1 minúscula, 1 mayuscula, 1 carac especial y min 6
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,}$")
+    private String password;
+
+    @NotBlank
+    @Size(min = 22, max = 22)
+    private String cvuMercadoPago;
+
+    @NotBlank
+    @Size(min = 8, max = 8)
+    private String walletCripto;
+}
