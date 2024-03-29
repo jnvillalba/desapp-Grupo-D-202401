b05a745bb0de755e5c4b49d975fb04a32933e09a
Joaquin Villalba <joaquinvillalba8@gmail.com>
2024-03-29 14:20:48 -0300

    refactor action

--- backend-desapp-api/src/main/java/ar/edu/unq/desapp/grupoD/backenddesappapi/model/User.java
+++ backend-desapp-api/src/main/java/ar/edu/unq/desapp/grupoD/backenddesappapi/model/User.java
@@ -5,12 +5,14 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class User {
 
     @NotBlank
@@ -42,4 +44,5 @@ public class User {
     @NotBlank
     @Size(min = 8, max = 8)
     private String walletCripto;
+
 }
