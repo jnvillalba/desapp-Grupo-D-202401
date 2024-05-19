c3a59ac777bf63cc69d513c38250492d4a8c6d07
Joaquin Villalba <joaquinvillalba8@gmail.com>
2024-05-19 20:55:20 -0300

    add getter to UserDTO

--- backend-desapp-api/src/main/java/ar/edu/unq/desapp/grupoD/backenddesappapi/model/dto/UserDTO.java
+++ backend-desapp-api/src/main/java/ar/edu/unq/desapp/grupoD/backenddesappapi/model/dto/UserDTO.java
@@ -5,8 +5,9 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
 import lombok.Setter;
-
+@Getter
 @Setter
 public class UserDTO {
 
