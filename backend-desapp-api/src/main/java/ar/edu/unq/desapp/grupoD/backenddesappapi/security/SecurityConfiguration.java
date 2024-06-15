ab51dee75568e3fe971ef26a59858d27f7a70b4b
Joaquin Villalba <joaquinvillalba8@gmail.com>
2024-06-15 16:09:22 -0300

    SecurityConfiguration

---
+++ backend-desapp-api/src/main/java/ar/edu/unq/desapp/grupoD/backenddesappapi/security/SecurityConfiguration.java
@@ -0,0 +1,39 @@
+package ar.edu.unq.desapp.grupod.backenddesappapi.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.http.SessionCreationPolicy;
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers(antMatcher("/swagger-ui/**"),
+                                    antMatcher("/swagger-ui.html"),
+                                    antMatcher("/v3/**"),
+                                    antMatcher("/h2-console/**"),
+                                    antMatcher("/console/**")).permitAll()
+                            .requestMatchers(antMatcher(HttpMethod.GET, "/users")).permitAll()
+                            .requestMatchers(antMatcher(HttpMethod.GET, "/intentions")).permitAll()
+                            //TODO: considera pasar role a enum
+                            .anyRequest().authenticated();
+                })
+                .headers(h -> h.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)) // This so embedded frames in h2-console are working
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return httpSecurity.build();
+    }
+
+}
